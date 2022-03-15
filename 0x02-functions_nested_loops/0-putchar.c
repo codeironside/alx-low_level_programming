644073955c8bd4af5be2589ba5e7fd8fb13060cb
codeironside <fury25423@gmail.com>
2022-03-15 16:21:03 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-  char word[9]="Holberton";
-  int i;
+  char word[9]="_putchar";
+  int i=0;
   
-  for(i =0; 1<9; i++)
-      _putchar(word[i]);
+  for(i = 0; 1 < 9; i++)
+  {
+      _putchar(text[i]);
+  }
   _putchar('\n');
   
   return (0);
