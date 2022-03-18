4f90126bca84d0dcbaadee6b73860609b3a7b2c3
codeironside <fury25423@gmail.com>
2022-03-18 12:57:18 +0100

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h
+#include "main.h"
 
 /**
  * print_remaining_days - takes a date and prints how many days are
@@ -29,7 +29,7 @@
  printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
  }
  else
- {c
+ {
  printf("Day of the year: %d/n", day);
  printf("Remaining days: %d\n", 365 - day);
  }
